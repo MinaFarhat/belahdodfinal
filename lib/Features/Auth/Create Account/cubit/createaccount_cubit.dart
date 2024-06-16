01726cf9340362164f7143708117a9aeb52e69d0
MinaFarhat <Mina.farhat333@gmail.com>
2024-06-16 08:57:45 +0300

    all files test7

---
+++ lib/Features/Auth/Create Account/cubit/createaccount_cubit.dart
@@ -0,0 +1,36 @@
+import 'package:belahododfinal/Core/error/network_exceptions.dart';
+import 'package:belahododfinal/Features/Auth/Create%20Account/Data/Model/createaccountentity.dart';
+import 'package:belahododfinal/Features/Auth/Create%20Account/Data/Repostry/basereposotrycreateaccount.dart';
+import 'package:bloc/bloc.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:injectable/injectable.dart';
+
+part 'createaccount_state.dart';
+part 'createaccount_cubit.freezed.dart';
+
+@injectable
+class CreateaccountCubit extends Cubit<CreateaccountState> {
+  CreateaccountCubit(this._baseReposotryCreateAccount)
+      : super(const CreateaccountState.initial());
+
+  final BaseReposotryCreateAccount _baseReposotryCreateAccount;
+
+  Future<void> emitCreateAccount({
+    required String name,
+    required String password,
+    required String phoneNumber,
+    required String city,
+    required String address,
+  }) async {
+    emit(const CreateaccountState.loading());
+    final response = await _baseReposotryCreateAccount.createaccount(
+      name,
+      password,
+      phoneNumber,
+      city,
+      address,
+    );
+    response.fold((l) => emit(CreateaccountState.error(l)),
+        (r) => emit(CreateaccountState.success(r)));
+  }
+}
---
+++ lib/Features/Auth/Create Account/cubit/createaccount_state.dart
@@ -0,0 +1,12 @@
+part of 'createaccount_cubit.dart';
+
+@freezed
+class CreateaccountState with _$CreateaccountState {
+  const factory CreateaccountState.initial() = _Initial;
+  const factory CreateaccountState.loading() = _Loading;
+  const factory CreateaccountState.success(
+          CreateAccountEntity createaccountentity) =
+      _Success;
+  const factory CreateaccountState.error(
+      NetworkExceptions networkExceptions) = _Error;
+}
