3921d807b54ae1b1be1db2d4907238838e5e704d
MinaFarhat <Mina.farhat333@gmail.com>
2024-08-02 12:40:01 +0300

    allfiles 86

--- lib/Core/api/end_points.dart
+++ lib/Core/api/end_points.dart
@@ -1,5 +1,5 @@
 class EndPoints {
-  static const String baseUrl = "http://192.168.1.105:8000/api";
+  static const String baseUrl = "http://10.0.2.2:8000/api";
   static const String createAccountUrl = "$baseUrl/register";
   static const String loginUrl = "$baseUrl/login";
   static const String addToCartUrl = "$baseUrl/cart/";
